6a27e2d71a034a5c3a9838b38ab68c07af9eff54
youssefescobar <coolgoot123@gmail.com>
2025-12-16 23:00:57 +0200

    a

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public', { index: 'main.html' })); 
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
+
 app.use('/api', allRoutes);
 
 
